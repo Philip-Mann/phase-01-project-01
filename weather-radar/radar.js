aad78468c6e155d2f108e98c557430c2d61786d6
LoganSutter44 <loganpsutter@gmail.com>
2021-05-01 21:00:30 -0400

    zooms in further on your location

--- weather-radar/radar.js
+++ weather-radar/radar.js
@@ -1,6 +1,6 @@
 var map = L.map('mapid').setView([38.9072, -77.0369], 6);
 function doMap() {
-    map.setView([lat, lon], 6);
+    map.setView([lat, lon], 10);
     //In setView make coordinates a variable so it will zoom in on current location
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
